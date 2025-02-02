7d67fff8eec29e298c5a7d4dc3a5b65058937bb1
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-02-02 23:26:44 +0400

    style: style pokemon card

--- src/components/pokemon-card/index.tsx
+++ src/components/pokemon-card/index.tsx
@@ -11,11 +11,13 @@ export class PokemonCard extends React.Component<IPokemonCardProps> {
     const { id, types, image, name, height, weight } = this.props.pokemon;
 
     return (
-      <article style={{ border: '1px solid red' }}>
-        <div>#{id}</div>
+      <article className="rounded-md border-2 border-red-50 p-4 shadow-md m-6 w-1/3 min-w-32 grow shrink-0">
+        <div className="text-sm text-right">#{id}</div>
+        <div>
+          Name: <span className="font-bold">{name}</span>
+        </div>
+        <img src={image} alt={name} className="mx-auto" />
         <div>Type: {types}</div>
-        <img src={image} alt={name} />
-        <div>Name: {name}</div>
         <div>Height: {height}</div>
         <div>Weight: {weight}</div>
       </article>
