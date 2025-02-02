e4c13d0af44c6d4e9ae6a3942e2b9c3ab58d816c
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-02-03 00:35:36 +0400

    fix(components): update pokemon card types

--- src/components/pokemon-card/index.tsx
+++ src/components/pokemon-card/index.tsx
@@ -7,7 +7,16 @@ interface IPokemonCardProps {
 
 export class PokemonCard extends React.Component<IPokemonCardProps> {
   render() {
-    const { id, types, image, name, height, weight } = this.props.pokemon;
+    const {
+      id,
+      types: typesArray,
+      image,
+      name,
+      height,
+      weight,
+    } = this.props.pokemon;
+
+    const types = typesArray.join(', ');
 
     return (
       <article className="rounded-md border-2 border-red-50 p-4 shadow-md m-6 w-1/3 min-w-32 grow shrink-0">
