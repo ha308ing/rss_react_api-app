ee5f2196c8661d8d3370946662e126670427398c
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-02-04 23:06:31 +0400

    refactor(components): update pokemon card to functional component

--- src/components/pokemon-card/index.tsx
+++ src/components/pokemon-card/index.tsx
@@ -1,34 +1,25 @@
 import { IPokemon } from '@/types';
-import React from 'react';
+import type { FC } from 'react';
 
 interface IPokemonCardProps {
   pokemon: IPokemon;
 }
 
-export class PokemonCard extends React.Component<IPokemonCardProps> {
-  render() {
-    const {
-      id,
-      types: typesArray,
-      image,
-      name,
-      height,
-      weight,
-    } = this.props.pokemon;
+export const PokemonCard: FC<IPokemonCardProps> = ({ pokemon }) => {
+  const { id, types: typesArray, image, name, height, weight } = pokemon;
 
-    const types = typesArray.join(', ');
+  const types = typesArray.join(', ');
 
-    return (
-      <article className="rounded-md border-2 border-red-50 p-4 shadow-md m-6 w-1/3 min-w-32 grow shrink-0">
-        <div className="text-sm text-right">#{id}</div>
-        <div>
-          Name: <span className="font-bold">{name}</span>
-        </div>
-        <img src={image} alt={name} className="mx-auto" />
-        <div>Type: {types}</div>
-        <div>Height: {height}</div>
-        <div>Weight: {weight}</div>
-      </article>
-    );
-  }
-}
+  return (
+    <article className="rounded-md border-2 border-red-50 p-4 shadow-md m-6 w-1/3 min-w-32 grow shrink-0">
+      <div className="text-sm text-right">#{id}</div>
+      <div>
+        Name: <span className="font-bold">{name}</span>
+      </div>
+      <img src={image} alt={name} className="mx-auto" />
+      <div>Type: {types}</div>
+      <div>Height: {height}</div>
+      <div>Weight: {weight}</div>
+    </article>
+  );
+};
