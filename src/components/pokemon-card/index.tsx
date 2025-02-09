dc6f49bb12363ae9f817ef44d413f851eeeb0a63
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-02-10 00:57:50 +0400

    feat(components): add pokemon card size and clickhandler props

--- src/components/pokemon-card/index.tsx
+++ src/components/pokemon-card/index.tsx
@@ -3,15 +3,26 @@ import type { FC } from 'react';
 
 interface IPokemonCardProps {
   pokemon: IPokemon;
+  size?: 'small' | 'big';
+  clickHandler?: () => void;
 }
 
-export const PokemonCard: FC<IPokemonCardProps> = ({ pokemon }) => {
+export const PokemonCard: FC<IPokemonCardProps> = ({
+  pokemon,
+  size = 'small',
+  clickHandler = undefined,
+}) => {
   const { id, types: typesArray, image, name, height, weight } = pokemon;
 
   const types = typesArray.join(', ');
 
+  const className =
+    size === 'big'
+      ? ''
+      : 'rounded-md border-2 border-red-50 p-4 shadow-md m-6 w-1/3 min-w-32 grow shrink-0';
+
   return (
-    <article className="rounded-md border-2 border-red-50 p-4 shadow-md m-6 w-1/3 min-w-32 grow shrink-0">
+    <article className={className} onClick={clickHandler}>
       <div className="text-sm text-right">#{id}</div>
       <div>
         Name: <span className="font-bold">{name}</span>
